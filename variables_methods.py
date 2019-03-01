60ab16bc3e2ca717ffca71dba1b2085cf21995ca
Alex Brown <alexraybrown232@gmail.com>
2019-02-28 19:09:15 -0800

    On branch master Initial Python Recap
    First time using Git. I chose to start alongside a class
    I'm taking on Rest API's at https://www.udemy.com/rest-api-flask-and-python/
    
    ---If you're a visitor, feel free to commit changes if you
    ---think I could be doing something fancier. Just make
    ---sure to leave detailed comments.---
    
    More to Come! ^v^
    Changes to be committed:
      new file:   variables_exercise.py
    	new file:   variables_methods.py
    Changes not staged for commit:
    	modified:   variables_methods.py

---
+++ variables_methods.py
@@ -0,0 +1,18 @@
+#####Python Refresher Notes #####
+
+###Variables
+#Essentially "Boxes" we can stick values in
+a = 5
+b = 10
+my_variable = 56
+my_10_variable = 10 #Gross...
+
+#String variables
+string_variable ="hello"
+single_quotes ="stirngs can have single quotes"
+
+##Printing
+#Method which accepts value and prints to console
+print(my_variable)
+print(string_variable)
+print(123)
